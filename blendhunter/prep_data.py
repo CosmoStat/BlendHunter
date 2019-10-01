4d028093868348b518c30d31012648c676d3242e
Alice Lacan <alice.b.lacan@gmail.com>
2019-10-01 11:40:35 +0200

    clean

--- blendhunter/prep_data.py
+++ blendhunter/prep_data.py
@@ -9,39 +9,31 @@ sys.path.extend([bh_path])
 from blendhunter.data import CreateTrainData
 
 #Function to get images
-def get_images(sample):
+def get_images(sample, add_noise = False, sigma_noise = None):
 
-    return np.array([sample[obj]['galsim_image'][0].array for obj in
+    if add_noise:
+        #Add noise to image and store array in dict
+        for i in range(len(sample)):
+            sample[i]['galsim_image_noisy'] = add_noise(sample[i]['galsim_image'][0].array, sigma = sigma_noise)
+
+        return np.array([sample[obj]['galsim_image_noisy'] for obj in
+                         range(sample.size)])
+    else:
+        return np.array([sample[obj]['galsim_image'][0].array for obj in
                      range(sample.size)])
 
-#Second function to obtain noisy images
-def get_images_noisy(sample):
-
-    return np.array([sample[obj]['galsim_image_noisy'] for obj in
-                     range(sample.size)])
 
 #Paths for datasets with different sigma_noise
 path = '/Users/alacan/Documents/Cosmostat/Codes/BlendHunter'
 input = path + '/axel_sims/larger_dataset'
+input_real = path + '/axel_sims/deblending_real'
+output_real = path + '/bh_real'
 output = path + '/bh'
-output2 = path + '/bh_5'
-output3 = path + '/bh_14'
-output4 = path + '/bh_18'
-output5 = path + '/bh_22'
-output6 = path + '/bh_26'
-output7 = path + '/bh_35'
-output8 = path + '/bh_40'
-
+output44 = path + '/more_noise_ranges/bh_44'
 
 #Getting the images
-blended = np.load(input + '/blended/gal_obj_0.npy', allow_pickle=True)
-not_blended = np.load(input + '/not_blended/gal_obj_0.npy', allow_pickle=True)
-
-#Add noise to images and store it in dict
-for i in range(len(blended)):
-    blended[i]['galsim_image_noisy'] = add_noise(blended[i]['galsim_image'][0].array, sigma = 35.0)
-for i in range(len(not_blended)):
-    not_blended[i]['galsim_image_noisy'] = add_noise(not_blended[i]['galsim_image'][0].array, sigma = 35.0)
+blended = np.load(input_real + '/blend/gal_obj_0.npy', allow_pickle=True)
+not_blended = np.load(input_real + '/no_blend/gal_obj_0.npy', allow_pickle=True)
 
 
 #Getting the psf (fwhm)
@@ -58,17 +50,12 @@ param_y = [np.array([blended[val]['blend_param']['dy'] for val in range(blended.
 #Getting the segmentation map
 sm = [np.array([blended[val]['seg_map'][0].array for val in range(blended.size)])]
 
-#Reshape
-#shift_params = [np.reshape(shift_params, (10000,1))]
-#shift_params = [np.concatenate((param_x, param_y), axis=1)]
+#Get images
+images = [get_images(sample) for sample in (blended, not_blended)]
 
-images = [get_images_noisy(sample) for sample in (blended, not_blended)]
+#Save noisy images for comparison w/ SExtractor
+#np.save(output_real+'/blended_noisy.npy', blended)
+#np.save(output_real+'/not_blended_noisy.npy', not_blended)
 
-#images_noisy = [add_noise(images[i], sigma = 22.0) for i in range(len(images))]
-
-#Save noisy images for comparison w/ Sextractor
-np.save(output7+'/blended_noisy.npy', blended)
-np.save(output7+'/not_blended_noisy.npy', not_blended)
-
-
-#CreateTrainData(images, output8).prep_axel(output8, fwhm, param_x, param_y, sm)
+#Train-valid-test split
+CreateTrainData(images, output_real).prep_axel(path_to_output=output_real, map=sm, param_1=param_x, param_2=param_y, psf=fwhm)
