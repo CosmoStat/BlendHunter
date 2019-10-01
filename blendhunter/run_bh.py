cd19c6b11c0e48029a9cba77381f5ad12fb5f9ff
Alice Lacan <alice.b.lacan@gmail.com>
2019-10-01 11:43:54 +0200

    clean

--- blendhunter/run_bh.py
+++ blendhunter/run_bh.py
@@ -11,10 +11,10 @@ sys.path.extend([bh_path])
 from blendhunter import BlendHunter
 
 
-path = '/Users/alacan/Documents/Cosmostat/Codes/BlendHunter/bh'
-#path_test = '/Users/alacan/Documents/Cosmostat/Codes/BlendHunter/bh_5'
+#path = '/Users/alacan/Documents/Cosmostat/Codes/BlendHunter/bh_22'
+path = '/Users/alacan/Documents/Cosmostat/Codes/BlendHunter/bh_real'
 
-bh = BlendHunter(weights_path=path + '/weights')
+bh = BlendHunter(weights_path=path + '/weights22')
 
 # Train Network
 bh.train(path + '/BlendHunterData',
@@ -25,17 +25,14 @@ hist = np.array(bh.history.history)
 
 # Predict Results
 pred_top = bh.predict(path + '/BlendHunterData/test/test', weights_type='top')
-pred_fine = bh.predict(path + '/BlendHunterData/test/test',
-                       weights_type='fine')
+#pred_fine = bh.predict(path + '/BlendHunterData/test/test', weights_type='fine')
 true = np.load(path + '/BlendHunterData/test/test/labels.npy')
 print("Match Top:", np.sum(pred_top == true) / true.size)
 #print(bh.history.history.keys())
-
-print("Match Fine:", np.sum(pred_fine == true) / true.size)
-
+#print("Match Fine:", np.sum(pred_fine == true) / true.size)
 print("Error Top", np.sum(pred_top != true) / true.size)
 
 #Save results
 np.save(path+'/BlendHunterData/test/test/history.npy', hist)
-np.save(path+'/BlendHunterData/test/test/pred_top_tune.npy', pred_top)
+np.save(path+'/BlendHunterData/test/test/pred_top22.npy', pred_top)
 #np.save('/Users/alacan/Documents/Cosmostat/Codes/BlendHunter/bh/BlendHunterData/test/test/pred_finetune.npy', pred_fine)
