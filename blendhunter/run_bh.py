81f9121fceb1a2365f9cef1399b61484ace173f2
Alice <alice.b.lacan@gmail.com>
2020-01-03 19:29:38 +0100

    clean

--- blendhunter/run_bh.py
+++ blendhunter/run_bh.py
@@ -5,23 +5,24 @@ bh_path = ('/Users/alacan/Documents/Cosmostat/Codes/BlendHunter')
 sys.path.extend([bh_path])
 
 # Set plaidml backend for Keras before importing blendhunter
-#import plaidml.keras
-#plaidml.keras.install_backend()
+import plaidml.keras
+plaidml.keras.install_backend()
 
 from blendhunter import BlendHunter
+from os.path import expanduser
+user_home = expanduser("~")
 
+#Make sure to check the folder hierarchy
+path = user_home+'/Documents/Cosmostat/Codes/BlendHunter/bh_'
 
-#path = '/Users/alacan/Documents/Cosmostat/Codes/BlendHunter/bh_22'
-path = '/Users/alacan/Documents/Cosmostat/Codes/BlendHunter/bh_real'
-
-bh = BlendHunter(weights_path=path + '/weights22')
+bh = BlendHunter(weights_path=path + '/weights')
 
 # Train Network
 bh.train(path + '/BlendHunterData',
-         get_features=True,
-         train_top=True,
+         get_features=False,
+         train_top=False,
          fine_tune=False)
-hist = np.array(bh.history.history)
+#hist = np.array(bh.history.history)
 
 # Predict Results
 pred_top = bh.predict(path + '/BlendHunterData/test/test', weights_type='top')
@@ -33,6 +34,6 @@ print("Match Top:", np.sum(pred_top == true) / true.size)
 print("Error Top", np.sum(pred_top != true) / true.size)
 
 #Save results
-np.save(path+'/BlendHunterData/test/test/history.npy', hist)
-np.save(path+'/BlendHunterData/test/test/pred_top22.npy', pred_top)
+#np.save(path+'/BlendHunterData/test/test/history.npy', hist)
+np.save(path+'/BlendHunterData/test/test/pred.npy', pred_top)
 #np.save('/Users/alacan/Documents/Cosmostat/Codes/BlendHunter/bh/BlendHunterData/test/test/pred_finetune.npy', pred_fine)
