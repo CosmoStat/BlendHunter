44ab5ae5b20e6b464734653294decb1b879e2f94
Alice <alice.b.lacan@gmail.com>
2020-01-03 19:28:27 +0100

    clean

--- blendhunter/prep_data.py
+++ blendhunter/prep_data.py
@@ -2,60 +2,79 @@ import numpy as np
 import sys
 import modopt
 from modopt.signal.noise import add_noise
+from modopt.base.np_adjust import pad2d
 
-bh_path = ('/Users/alacan/Documents/Cosmostat/Codes/BlendHunter')
+from os.path import expanduser
+user_home = expanduser("~")
+
+#Check the folder hierarchy
+bh_path = (user_home+'/Documents/Cosmostat/Codes/BlendHunter')
 sys.path.extend([bh_path])
 
 from blendhunter.data import CreateTrainData
 
-#Function to get images
-def get_images(sample, add_noise = False, sigma_noise = None):
+#Function to add noise according to signal
+def noise_to_add(img=None, SNR_=None, map=None):
+    #Calculate std of central object signal
+    signal = np.sum(img[map == 1]**2)
+    # Calculate noise to add on image
+    std_noise = np.sqrt(signal) / SNR_
+    #add noise
+    return add_noise(img, sigma = std_noise)
 
-    if add_noise:
+#Function to get images
+def get_images(sample, add_noise_img = False, sigma_noise = None, add_padding_noise=False, fixed_snr = False, SNR_target=None, seg_map=None):
+
+    if add_noise_img:
         #Add noise to image and store array in dict
         for i in range(len(sample)):
             sample[i]['galsim_image_noisy'] = add_noise(sample[i]['galsim_image'][0].array, sigma = sigma_noise)
 
         return np.array([sample[obj]['galsim_image_noisy'] for obj in
                          range(sample.size)])
+
+    if add_padding_noise:
+        #Pad image 7 by 7
+        for i in range(len(sample)):
+            sample[i]['galsim_image_pad'] = pad2d(sample[i]['galsim_image'][0].array, (7, 7))
+
+        #Then add noise to image and store array in dict
+        for i in range(len(sample)):
+            sample[i]['galsim_image_noisy'] = add_noise(sample[i]['galsim_image_pad'], sigma = sigma_noise)
+
+        return np.array([sample[obj]['galsim_image_noisy'] for obj in
+                         range(sample.size)])
+    # Add noise according to a fixed SNR level to reach
+    if fixed_snr:
+        for i in range(len(sample)):
+            sample[i]['galsim_image_noisy'] = noise_to_add(img=sample[i]['galsim_image'][0].array, SNR_=SNR_target, map=seg_map[i])
+            #Normalise images
+            sample[i]['galsim_image_noisy'] /= sample[i]['galsim_image_noisy'].max()
+
+        return np.array([sample[obj]['galsim_image_noisy'] for obj in
+                         range(sample.size)])
+    #Return only non noisy image
     else:
         return np.array([sample[obj]['galsim_image'][0].array for obj in
                      range(sample.size)])
 
 
 #Paths for datasets with different sigma_noise
-path = '/Users/alacan/Documents/Cosmostat/Codes/BlendHunter'
+path = user_home+'/Documents/Cosmostat/Codes/BlendHunter'
+
 input = path + '/axel_sims/larger_dataset'
 input_real = path + '/axel_sims/deblending_real'
-output_real = path + '/bh_real'
-output = path + '/bh'
-output44 = path + '/more_noise_ranges/bh_44'
+output= path + '/bh_pad14'
 
 #Getting the images
-blended = np.load(input_real + '/blend/gal_obj_0.npy', allow_pickle=True)
-not_blended = np.load(input_real + '/no_blend/gal_obj_0.npy', allow_pickle=True)
-
-
-#Getting the psf (fwhm)
-fwhm_blended = np.array([blended[val]['PSF']['fwhm'] for val in range(blended.size)])
-fwhm_not_blended = np.array([not_blended[val]['PSF']['fwhm'] for val in range(not_blended.size)])
-
-#Final fwhm array
-fwhm = [fwhm_blended, fwhm_not_blended]
-
-#Getting the shift parameters
-param_x = [np.array([blended[val]['blend_param']['dx'] for val in range(blended.size)])]
-param_y = [np.array([blended[val]['blend_param']['dy'] for val in range(blended.size)])]
-
-#Getting the segmentation map
-sm = [np.array([blended[val]['seg_map'][0].array for val in range(blended.size)])]
+blended = np.load(input+ '/blended/gal_obj_0.npy', allow_pickle=True)
+not_blended = np.load(input+ '/not_blended/gal_obj_0.npy', allow_pickle=True)
 
 #Get images
-images = [get_images(sample) for sample in (blended, not_blended)]
-
+images = [get_images(sample, add_padding_noise = True, sigma_noise=14.0) for sample in (blended, not_blended)]
 #Save noisy images for comparison w/ SExtractor
-#np.save(output_real+'/blended_noisy.npy', blended)
-#np.save(output_real+'/not_blended_noisy.npy', not_blended)
+np.save(output+'/blended_noisy.npy', blended)
+np.save(output+'/not_blended_noisy.npy', not_blended)
 
 #Train-valid-test split
-CreateTrainData(images, output_real).prep_axel(path_to_output=output_real, map=sm, param_1=param_x, param_2=param_y, psf=fwhm)
+CreateTrainData(images, output).prep_axel(path_to_output=output)
