05d1d8aaf01914f61ea66f5565e74643a1ee96e5
Alice <alice.b.lacan@gmail.com>
2020-01-03 19:28:59 +0100

    clean

--- blendhunter/prep_data_loop.py
+++ blendhunter/prep_data_loop.py
@@ -4,21 +4,24 @@ import modopt
 from modopt.signal.noise import add_noise
 from modopt.base.np_adjust import pad2d
 
-bh_path = ('/Users/lacan/Documents/Cosmostat/Codes/BlendHunter')
+from os.path import expanduser
+user_home = expanduser("~")
+#Check for the folder hierarchy
+bh_path = (user_home+'/Documents/Cosmostat/Codes/BlendHunter')
 sys.path.extend([bh_path])
 
 from blendhunter.data import CreateTrainData
 
+#Function to calculate the amount of noise to add according to signal
 def noise_to_add(img=None, SNR_=None, map=None):
     #Calculate std of central object signal
     signal = np.sum(img[map == 1]**2)
     # Calculate noise to add on image
     std_noise = np.sqrt(signal) / SNR_
-    #add noise
     return add_noise(img, sigma = std_noise)
 
 #Function to get images
-def get_images(sample, add_noise_img = False, sigma_noise = None, extra_padding=False, add_pad_noise=False, fixed_snr = False, SNR_target=None, seg_map=None):
+def get_images(sample, add_noise_img = False, sigma_noise = None, add_padding_noise=False, fixed_snr = False, SNR_target=None, seg_map=None):
 
     if add_noise_img:
         #Add noise to image and store array in dict
@@ -27,16 +30,8 @@ def get_images(sample, add_noise_img = False, sigma_noise = None, extra_padding=
 
         return np.array([sample[obj]['galsim_image_noisy'] for obj in
                          range(sample.size)])
-
-    if extra_padding:
-        #Add extra padding to image and store in dict
-        for i in range(len(sample)):
-            sample[i]['galsim_image_pad'] = pad2d(sample[i]['galsim_image'][0].array, (7, 7))
-
-        return np.array([sample[obj]['galsim_image_pad'] for obj in
-                         range(sample.size)])
-
-    if add_pad_noise:
+    #Padding of 7 by 7 + noise
+    if add_padding_noise:
         #Pad image
         for i in range(len(sample)):
             sample[i]['galsim_image_pad'] = pad2d(sample[i]['galsim_image'][0].array, (7, 7))
@@ -47,7 +42,7 @@ def get_images(sample, add_noise_img = False, sigma_noise = None, extra_padding=
 
         return np.array([sample[obj]['galsim_image_noisy'] for obj in
                          range(sample.size)])
-
+    #Create images with fixed SNR
     if fixed_snr:
         for i in range(len(sample)):
             sample[i]['galsim_image_noisy'] = noise_to_add(img=sample[i]['galsim_image'][0].array, SNR_=SNR_target, map=seg_map[i])
@@ -56,17 +51,20 @@ def get_images(sample, add_noise_img = False, sigma_noise = None, extra_padding=
 
         return np.array([sample[obj]['galsim_image_noisy'] for obj in
                          range(sample.size)])
-
+    #Get non noisy images
     else:
         return np.array([sample[obj]['galsim_image'][0].array for obj in
                      range(sample.size)])
 
 
 #Paths for datasets with different sigma_noise
-path = '/Users/lacan/Documents/Cosmostat/Codes/BlendHunter'
+path = user_home+'/Documents/Cosmostat/Codes/BlendHunter'
+
 input = path + '/axel_sims/larger_dataset'
 input_real = path + '/axel_sims/deblending_real'
-output= path + '/bh_{}'
+
+#Dataset called bh_+ the noise level + the number of noise realisation
+output = path + '/bh_{}'
 
 #Getting the images
 blended = np.load(input+ '/blended/gal_obj_0.npy', allow_pickle=True)
@@ -75,7 +73,7 @@ not_blended = np.load(input+ '/not_blended/gal_obj_0.npy', allow_pickle=True)
 for j in [5, 14, 18, 26, 35, 40]:
     for i in [1,2,3,4]:
         #Get images
-        images = [get_images(sample, add_pad_noise = True, sigma_noise=j) for sample in (blended, not_blended)]
+        images = [get_images(sample, add_padding_noise = True, sigma_noise=j) for sample in (blended, not_blended)]
         #Save noisy images for comparison w/ SExtractor
         np.save(output.format(str(j)+str(i))+'/blended_noisy{}.npy'.format(str(j)+str(i)), blended[36000:40000])
         np.save(output.format(str(j)+str(i))+'/not_blended_noisy{}.npy'.format(str(j)+str(i)), not_blended[36000:40000])
