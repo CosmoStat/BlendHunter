99c50d572cebc219afd8dfb8d8bbe7888838f78d
Alice <alice.b.lacan@gmail.com>
2020-01-03 19:30:06 +0100

    clean

--- blendhunter/run_bh_loop.py
+++ blendhunter/run_bh_loop.py
@@ -9,11 +9,14 @@ import plaidml.keras
 plaidml.keras.install_backend()
 
 from blendhunter import BlendHunter
+from os.path import expanduser
+user_home = expanduser("~")
 
 
 for j in [5, 14, 18, 26, 35, 40]:
     for i in [1,2,3,4]:
-        path = '/Users/lacan/Documents/Cosmostat/Codes/BlendHunter/bh_{}'.format(str(j)+str(i))
+        #Make sure to check the folder hierarchy
+        path = user_home+'/Documents/Cosmostat/Codes/BlendHunter/bh_{}'.format(str(j)+str(i))
         bh = BlendHunter(weights_path=path + '/weights')
 
         # Train Network
@@ -33,6 +36,7 @@ for j in [5, 14, 18, 26, 35, 40]:
         #print("Match Fine:", np.sum(pred_fine == true) / true.size)
         print("Error Top", np.sum(pred_top != true) / true.size)
 
-        #Save results
+        #Save history and results
+        #Make sure to check the folder hierarchy
         np.save(path+'/BlendHunterData/test/test/history.npy', hist)
-        np.save('/Users/lacan/Documents/Cosmostat/Codes/BlendHunter/pad_results/preds_pad{}.npy'.format(str(j)+str(i)), pred_top)
+        np.save(user_home+'/Documents/Cosmostat/Codes/BlendHunter/pad_results/preds_pad{}.npy'.format(str(j)+str(i)), pred_top)
