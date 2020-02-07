f6e960940d23199fb8ff9bc2f329af7a2fcc48a7
Alice <alice.b.lacan@gmail.com>
2020-02-07 02:29:31 +0100

    clean

--- blendhunter/run_bh_loop.py
+++ blendhunter/run_bh_loop.py
@@ -1,34 +1,36 @@
 import numpy as np
 import sys
-
-bh_path = ('/Users/lacan/Documents/Cosmostat/Codes/BlendHunter')
-sys.path.extend([bh_path])
-
-# Set plaidml backend for Keras before importing blendhunter
-import plaidml.keras
-plaidml.keras.install_backend()
-
-from blendhunter import BlendHunter
 from os.path import expanduser
 user_home = expanduser("~")
 
+#Check the folder hierarchy
+bh_path = (user_home+'/Documents/Cosmostat/Codes/BlendHunter')
+sys.path.extend([bh_path])
 
+"""Set plaidml backend for Keras before importing blendhunter"""
+import plaidml.keras
+plaidml.keras.install_backend()
+
+#Import network
+from blendhunter import BlendHunter
+
+"""Loop to train the network on padded noisy images and get predicted labels"""
 for j in [5, 14, 18, 26, 35, 40]:
-    for i in [1,2,3,4]:
+    for i in ['', 1,2,3,4]:
         #Make sure to check the folder hierarchy
         path = user_home+'/Documents/Cosmostat/Codes/BlendHunter/bh_{}'.format(str(j)+str(i))
+        """Make sure each bh folder contains a 'weights' folder"""
         bh = BlendHunter(weights_path=path + '/weights')
 
         # Train Network
-
         bh.train(path + '/BlendHunterData',
          get_features=True,
          train_top=True,
-         fine_tune=False)
+         fine_tune=False) #No fine tuning
 
-        hist = np.array(bh.history.history)
+        hist = np.array(bh.history.history) #Saving training history is optional
 
-         # Predict Results
+        # Predict Results
         pred_top = bh.predict(path + '/BlendHunterData/test/test', weights_type='top')
         #pred_fine = bh.predict(path + '/BlendHunterData/test/test', weights_type='fine')
         true = np.load(path + '/BlendHunterData/test/test/labels.npy')
