ace4103c839d5431fcd08fbfe99979a4afb265c3
Alice <alice.b.lacan@gmail.com>
2020-02-10 04:23:25 +0100

    clean

--- folders_creation.py
+++ folders_creation.py
@@ -6,7 +6,7 @@ import os
 from os.path import expanduser
 user_home = expanduser("~")
 
-main_path = user_home+'/Documents/Cosmostat/Codes/BlendHunter'
+main_path = user_home+'/Cosmostat/Codes/BlendHunter'
 
 
 """Generate 35 folders for non padded images"""
@@ -65,23 +65,23 @@ os.mkdir(sep_pad_results)
 
 
 """Generate more datasets to test the weights (for both padded and non padded images)"""
-more_noise_ranges= main_path+'/more_noise_ranges'
-more_noise_ranges_pad= main_path+'/more_noise_ranges_pad'
+#more_noise_ranges= main_path+'/more_noise_ranges'
+#more_noise_ranges_pad= main_path+'/more_noise_ranges_pad'
 
-if os.path.isdir(more_noise_ranges):
-    raise FileExistsError('{} already exists. Please remove this '
+#if os.path.isdir(more_noise_ranges):
+#    raise FileExistsError('{} already exists. Please remove this '
                   'directory or choose a new path.'
-                  ''.format(more_noise_ranges))
-if os.path.isdir(more_noise_ranges_pad):
-    raise FileExistsError('{} already exists. Please remove this directory or choose a new path.'
-                  ''.format(more_noise_ranges_pad))
+#                  ''.format(more_noise_ranges))
+#if os.path.isdir(more_noise_ranges_pad):
+#    raise FileExistsError('{} already exists. Please remove this directory or choose a new path.'
+#                  ''.format(more_noise_ranges_pad))
 
-os.mkdir(more_noise_ranges)
-os.mkdir(more_noise_ranges_pad)
+#os.mkdir(more_noise_ranges)
+#os.mkdir(more_noise_ranges_pad)
 
 """Generate the folders for new test sets (for both padded and non padded images)"""
 for i in [3,7,10,12,16,20,22,24,28,30,32,37,42,44]:
-    bh_path =  main_path+'/more_noise_ranges/bh_{}'.format(i)
+    bh_path =  main_path+'/bh_{}'.format(i)
 
     if os.path.isdir(bh_path):
         raise FileExistsError('{} already exists. Please remove this '
@@ -90,10 +90,47 @@ for i in [3,7,10,12,16,20,22,24,28,30,32,37,42,44]:
     os.mkdir(bh_path)
 
 for i in [3,7,10,12,16,20,22,24,28,30,32,37,42,44]:
-    bh_path =  main_path+'/more_noise_ranges_pad/bh_{}'.format(i)
+    bh_path =  main_path+'/bh_pad{}'.format(i)
 
     if os.path.isdir(bh_path):
         raise FileExistsError('{} already exists. Please remove this '
                       'directory or choose a new path.'
                       ''.format(bh_path))
     os.mkdir(bh_path)
+
+
+
+"""Generate the folder regrouping the pre-trained weights"""
+folder =  main_path+'/pretrained_weights'.format(i)
+folder_pad =  main_path+'/pretrained_weights_pad'.format(i)
+
+if os.path.isdir(folder):
+    raise FileExistsError('{} already exists. Please remove this '
+                      'directory or choose a new path.'
+                      ''.format(folder))
+if os.path.isdir(folder_pad):
+    raise FileExistsError('{} already exists. Please remove this '
+                      'directory or choose a new path.'
+                      ''.format(folder_pad))
+os.mkdir(folder)
+os.mkdir(folder_pad)
+
+
+
+
+
+"""Generate folders for real data (Cosmos images)"""
+real =  main_path+'/bh_real'.format(i)
+real_pad =  main_path+'/bh_real_pad'.format(i)
+
+if os.path.isdir(real):
+    raise FileExistsError('{} already exists. Please remove this '
+                      'directory or choose a new path.'
+                      ''.format(real))
+if os.path.isdir(real_pad):
+    raise FileExistsError('{} already exists. Please remove this '
+                      'directory or choose a new path.'
+                      ''.format(real_pad))
+
+os.mkdir(real)
+os.mkdir(real_pad)
