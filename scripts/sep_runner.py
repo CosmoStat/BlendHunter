06d775387b3111ea9055dd8c649386d1930df209
Axel <axel.guinot@cea.fr>
2020-03-03 14:56:37 +0100

    Update sep_runner
    New SExtractor classification

--- scripts/sep_runner.py
+++ scripts/sep_runner.py
@@ -105,8 +105,8 @@ class Run_Sep:
     def check_blend(self, res):
         """ Check blend
 
-        Check if objects identify by sep.extract are flagged as blended by
-        SExtractor.
+        Check if at least one object identify by sep.extract is flagged as 
+        blended by SExtractor.
 
         Parameters
         ----------
@@ -115,103 +115,17 @@ class Run_Sep:
 
         Returns
         -------
-        flags : list
-            List of the flag for each objects. flag = 1 if the object is
-            blended and 0 otherwise.
+        flag : bool
+            flag = 1 if the object is blended and 0 otherwise.
 
         """
 
         n_obj = len(res)
 
-        flags = [1 if sep.OBJ_MERGED in self.get_power_2(res[i]['flag'])
-                 else 0 for i in range(n_obj)]
+        flag = np.array([1 if sep.OBJ_MERGED in self.get_power_2(res[i]['flag'])
+                 else 0 for i in range(n_obj)], dtype=bool).sum()
 
-        return flags
-
-    @staticmethod
-    def compute_dist(obj_res, obj_true_pos):
-        """ Compute distance
-
-        Compute the euclidean distance between 2 points.
-
-        Parameters
-        ----------
-        obj_res : numpy.ndarray
-            Structure array return by sep.extract method for one object.
-        obj_true_pos : list
-            List containing the true position. (example : [x, y])
-
-        Returns
-        -------
-        dist : float
-            Euclidean distance between 2 points.
-
-        """
-
-        dist = np.sqrt((obj_res['x'] - obj_true_pos[0])**2. +
-                       (obj_res['y'] - obj_true_pos[1])**2.)
-
-        return dist
-
-    def get_dist(self, res, true_pos, thresh=2):
-        """ Get distance
-
-        This method return the distance between the real objects and the best
-        match find in the SExtractor output. If the distance is larger than the
-        the threshold seted then the object is flag.
-
-        Parameters
-        ----------
-        res : list
-            Output of the sep.extract method.
-        true_pos : list
-            List of the true positions (example : [[x1, y1], [x2, y2], ...]
-        thresh : float
-            Maximum distance allowed for a match (default = 2)
-
-        Returns
-        -------
-        numpy.ndarray
-            Array containing all the distances.
-
-        """
-
-        dists = []
-        ind_used = []
-        for i in range(len(true_pos)):
-            tmp = self.BIG_DISTANCE
-            for j in range(len(res)):
-                tmp2 = self.compute_dist(res[j], true_pos[i])
-                ind_tmp = -10
-                if (tmp2 < tmp) and (j not in ind_used) and (tmp2 < thresh):
-                    tmp = tmp2
-                    ind_tmp = j
-            ind_used.append(ind_tmp)
-            dists.append(tmp)
-
-        return np.array(dists)
-
-    def _check_dist(self, dists):
-        """ Check distance
-
-        Flag a list of object depending if SExtractor well identify them or
-        not. If at least one object in the list is not well identify will
-        return False.
-
-        Parameters
-        ----------
-        dists : list
-            List of distances return by the method get_dist
-
-        Returns
-        int
-            1 if all the objects are good and 0 otherwise
-
-        """
-
-        return np.prod([1 if dist < self.BIG_DISTANCE else 0
-                        for dist in dists],
-                       dtype=bool).astype(int)
+        return flag
 
     def process(self, f, epoch=0):
         """ Process
@@ -253,62 +167,22 @@ class Run_Sep:
             # Retrieve denoised image
             # img = obj['denoised_img']
 
-            true_nobj = 1
-            if obj['blended']:
-                true_nobj += len(obj['blend_param']['dx'])
-
             res = self.run_sep(img)
-
-            obj_pos = [[img.shape[0] / 2., img.shape[1] / 2.]]
 
             if len(res) == 0:
                 blend_tmp = self.MISS_EXTRACTION
 
             elif len(res) == 1:
                 if obj['blended']:
-                    obj_pos += [[obj_pos[0][0] + obj['blend_param']['dx'][i],
-                                 obj_pos[0][1] + obj['blend_param']['dy'][i]]
-                                for i in range(true_nobj - 1)]
-                dists_tmp = self.get_dist(res, obj_pos)
-                flag = self.check_blend(res)
-                if (flag[0]) and self._check_dist(dists_tmp):
-                    blend_tmp = self.BLEND
-                else:
-                    blend_tmp = self.NO_BLEND
-            elif len(res) == true_nobj:
-                obj_pos += [[obj_pos[0][0] + obj['blend_param']['dx'][i],
-                             obj_pos[0][1] + obj['blend_param']['dy'][i]]
-                            for i in range(true_nobj - 1)]
-                dists_tmp = self.get_dist(res, obj_pos)
-                flag = self.check_blend(res)
-                if self._check_dist(dists_tmp):
-                    blend_tmp = self.BLEND
-                else:
-                    if np.sum([1 if flag[i] and
-                               self._check_dist([dists_tmp[i]]) else 0
-                               for i in range(true_nobj)], dtype=bool):
+                    if self.check_blend(res):
                         blend_tmp = self.BLEND
                     else:
                         blend_tmp = self.NO_BLEND
-            else:
-                if obj['blended']:
-                    obj_pos += [[obj_pos[0][0] + obj['blend_param']['dx'][i],
-                                 obj_pos[0][1] + obj['blend_param']['dy'][i]]
-                                for i in range(true_nobj-1)]
-                dists_tmp = self.get_dist(res, obj_pos)
-                flag = self.check_blend(res)
-                n_good_dist = np.sum([1 if self._check_dist([dists_tmp[i]])
-                                      else 0 for i in range(true_nobj)],
-                                     dtype=int)
-                if n_good_dist == true_nobj:
-                    blend_tmp = self.BLEND
-                elif n_good_dist > true_nobj:
-                    blend_tmp = self.MISS_EXTRACTION
-                elif np.sum([1 if flag[i] and self._check_dist([dists_tmp[i]])
-                            else 0 for i in range(true_nobj)], dtype=bool):
-                    blend_tmp = self.BLEND
                 else:
                     blend_tmp = self.NO_BLEND
+            else:
+                blend_tmp = self.BLEND
+            
 
             blend_flag.append(blend_tmp)
 
